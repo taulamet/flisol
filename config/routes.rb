fbc9d1e8f5f0313b01c20c1f8f32c1530f5ed4a8
Juan Pablo Taulamet <taulamet@gmail.com>
2014-08-07 00:03:02 -0300

    Agregando Inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get '/inicio', to: redirect('/')
+
+  get 'inicio' => 'inicio#index'
+
   devise_for :usuarios
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
+
+root 'inicio#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
